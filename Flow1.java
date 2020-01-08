7e232a7e4c328b557a79629557062ef7f6676e37
Rajeshkumar Elumalai <38979122+elumalair@users.noreply.github.com>
2020-01-08 21:36:35 +0530

    Create Flow1.java

---
+++ Flow1.java
@@ -0,0 +1,17 @@
+public class TestResult {
+ 
+    public static void main(String[] args) {
+         
+        int marks = 45;
+         
+        if ( marks >= 50 ) {
+            System.out.println("Passed");
+        }
+        else
+        {
+            System.out.println("Failed");
+        }
+ 
+    }
+ 
+}
